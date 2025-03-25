7cfb0b607aefb4f6c310cf8d34247f19bc583071
Bubble Yu <bubbleyu@stanford.edu>
2025-03-25 15:59:25 -0700

    added index

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <main>
+        <h1>Hello from DA-STANFORD-FOREST 👋</h1>
+        <p>This is your landing page.</p>
+      </main>
+    );
+  }
+  
